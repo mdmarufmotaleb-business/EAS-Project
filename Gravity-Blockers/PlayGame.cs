9d357ae300e27038cdb988d77f4baa76babd383a
MD Maruf Motaleb <mdmarufmotaleb.business@gmail.com>
2026-09-21 22:58:24 +0100

    Fixed bug where third move wouldnt switch correctly

--- Gravity-Blockers/PlayGame.cs
+++ Gravity-Blockers/PlayGame.cs
@@ -11,27 +11,20 @@ namespace EAS_Project
 
             while (true) //Game loop starts here
             {
+                Display.DisplayMovesRemainingMessage(currentPlayer);
+                
                 if (currentPlayer.movesRemaining == 3 || currentPlayer.movesRemaining == 2)
                 {
-                    Display.DisplayMovesRemainingMessage(currentPlayer);
                     Display.DisplayMovePieceMessage();
-
                     PlayGame.MakeMove(grid, currentPlayer, playerA, playerB, false); // First and second moves are always PIECES
-                   
-                    Display.DisplayGrid(grid);
                 }
                 else if (currentPlayer.movesRemaining == 1)
                 {
-                    Display.DisplayMovesRemainingMessage(currentPlayer);
                     Display.DisplayMoveBlockMessage();
-
                     PlayGame.MakeMove(grid, currentPlayer, playerA, playerB, true); // Third move is always a BLOCK
-                    
-                    currentPlayer.ResetMoves();
-                    PlayGame.SwitchPlayer(ref currentPlayer, playerA, playerB);
-
-                    Display.DisplayGrid(grid);
                 }
+
+                Display.DisplayGrid(grid);
             }
 
         }
@@ -53,14 +46,14 @@ namespace EAS_Project
             Console.Write("\nAnswer: ");
             string? answer = Console.ReadLine();
 
-            if (answer == "RIGHT"){
+            if (answer?.ToUpper() == "RIGHT"){
                 grid.RotateRight();
 
                 Display.DisplayRotationMessage("RIGHT");
                 Display.DisplayTurnNameMessage(currentPlayer.name);
                 Display.DisplayTurnMessage();
             }
-            else if (answer == "LEFT"){
+            else if (answer?.ToUpper() == "LEFT"){
                 grid.RotateLeft();
 
                 Display.DisplayRotationMessage("LEFT");
@@ -79,6 +72,13 @@ namespace EAS_Project
                 {
                     currentPlayer.DecrementMoves();
                     grid.MakeMove(column, currentPlayer, playerA, playerB, isBlock);
+
+                    // Blocks are always dropped last, then switch players
+                    if (isBlock)
+                    {
+                        currentPlayer.ResetMoves();
+                        PlayGame.SwitchPlayer(ref currentPlayer, playerA, playerB);
+                    }
 
                     Display.DisplayDropSuccessMessage(currentPlayer.name, column, isBlock);
                     Display.DisplayTurnNameMessage(currentPlayer.name);
